d5f30f4ef462b4820df3bf9c798561337d5e3b09
Aryan Agrawal <aryanpranshu@gmail.com>
2019-04-06 00:11:15 +0530

    database

--- app/src/main/java/com/example/merchandiseapp/MainActivity.java
+++ app/src/main/java/com/example/merchandiseapp/MainActivity.java
@@ -5,10 +5,23 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
 
+
+
+
+    private Button btn;
+    public static final String Database_PATH = "Merchandise";
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -35,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // final DatabaseReference myRef = database.getReference();
+        //String recordId = myRef.push().getKey();
+        //myRef.child(recordId).setValue("Reebok");
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+
+                myRef.setValue("Hello, World!");
+            }
+        });
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
