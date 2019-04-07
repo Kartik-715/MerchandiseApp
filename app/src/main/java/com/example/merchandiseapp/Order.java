a0f0c0e01fc2849aa0434e36a54579670b41b0ee
Kartik Gupta <gupta170101030@iitg.ac.in>
2019-04-07 22:22:06 +0530

    Changed fields in order

--- app/src/main/java/com/example/merchandiseapp/Order.java
+++ app/src/main/java/com/example/merchandiseapp/Order.java
@@ -3,14 +3,14 @@ package com.example.merchandiseapp;
 import java.util.HashMap;
 
 public class Order {
-    private String userId;
+    private String buyer;
     private Merchandise item ;
     private Integer size ;
     private Integer quantity ;
     private boolean placed= false;
 
-    public Order(String userId, Merchandise item, Integer size, Integer quantity, boolean placed) {
-        this.userId = userId;
+    public Order(String buyer, Merchandise item, Integer size, Integer quantity, boolean placed) {
+        this.buyer = buyer;
         this.item = item;
         this.size = size;
         this.quantity = quantity;
@@ -23,8 +23,8 @@ public class Order {
     public HashMap<String, Object> toMap()
     {
         HashMap<String, Object> result = new HashMap<>() ;
-        result.put("userId", userId) ;
-        result.put("item", item) ;
+        result.put("buyer", buyer) ;
+        result.put("item", item.toMap()) ;
         result.put("size", size) ;
         result.put("quantity", quantity) ;
         result.put("placed", placed) ;
