494bc69a729e1d5962e2a1ea5495a237829f2f4a
Aryan Agrawal <aryanpranshu@gmail.com>
2019-04-08 12:01:07 +0530

    cardview added

--- app/src/main/java/com/example/merchandiseapp/HomeActivity.java
+++ app/src/main/java/com/example/merchandiseapp/HomeActivity.java
@@ -6,9 +6,13 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,32 +22,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DatabaseReference ProductRef;
+
     private static final String TAG = HomeActivity.class.getSimpleName();
     private Button btnLogout;
+    private RecyclerView rv;
+    private List<Merchandise> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
         final FirebaseAuth mauth = FirebaseAuth.getInstance();
         FirebaseUser user = mauth.getCurrentUser();
         Log.d("name",user.getDisplayName());
+        ProductRef = FirebaseDatabase.getInstance().getReference().child("Merchandise").child("Footwear");
+        String key =ProductRef.push().getKey();
+        ProductRef.child(key).setValue("Sample");
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -74,6 +99,63 @@ public class HomeActivity extends AppCompatActivity
         ImageView imageView =(ImageView) headerView.findViewById(R.id.imageView);
         new DownloadImageTask(imageView)
                 .execute(user.getPhotoUrl().toString());
+
+
+        rv = (RecyclerView) findViewById(R.id.recycler_menu);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        //rv.setHasFixedSize(true);
+
+        ProductRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list = new ArrayList<>();
+                Merchandise product = dataSnapshot.getValue(Merchandise.class);
+                String name=product.getBrandName();
+                Log.d(TAG,"***********sample stage*****************");
+                Log.d(TAG,name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG,"***********sample error stage*****************");
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"*****************stage1***************");
+        FirebaseRecyclerOptions<Merchandise> options =
+                new FirebaseRecyclerOptions.Builder<Merchandise>()
+                .setQuery(ProductRef, Merchandise.class)
+                .build();
+        Log.d(TAG,"*****************stage2***************");
+        FirebaseRecyclerAdapter<Merchandise, MyAdapter.MyViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Merchandise, MyAdapter.MyViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position, @NonNull Merchandise model) {
+                        Log.d(TAG,"*****************stage3***************");
+                        Log.d(TAG,model.getCategory());
+                        holder.productDescription.setText(model.getCategory());
+                        holder.productName.setText(model.getBrandName());
+                        holder.price.setText(model.getPrice().toString());
+                        Picasso.get().load(model.getImage()).into(holder.ImageProduct);
+                    }
+
+                    @NonNull
+                    @Override
+                    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        Log.d(TAG,"*****************stage4***************");
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list,viewGroup,false);
+                        MyAdapter.MyViewHolder holder = new MyAdapter.MyViewHolder(view);
+                        return holder;
+                    }
+                };
+        Log.d(TAG,"*****************stage5***************");
+        rv.setAdapter(adapter);
     }
 
     @Override
