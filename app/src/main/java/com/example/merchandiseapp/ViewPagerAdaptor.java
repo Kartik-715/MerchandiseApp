a0007898460115e0b6412020dc5722791708641d
Kartik Gupta <gupta170101030@iitg.ac.in>
2019-04-15 01:02:56 +0530

    Added Category wise View

---
+++ app/src/main/java/com/example/merchandiseapp/ViewPagerAdaptor.java
@@ -0,0 +1,42 @@
+package com.example.merchandiseapp;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdaptor extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>() ;
+    private final List<String> fragmentListTitle = new ArrayList<>() ;
+
+    public ViewPagerAdaptor(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i) ;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitle.get(position) ;
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentListTitle.size() ;
+    }
+
+    public void AddFragment(Fragment fragment, String title)
+    {
+        fragmentList.add(fragment) ;
+        fragmentListTitle.add(title) ;
+        notifyDataSetChanged();
+    }
+}
