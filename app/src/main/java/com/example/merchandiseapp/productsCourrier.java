b98ddef1b84314f89dcf7f634921c45f767e0f4e
mnk343 <mayank343@gmail.com>
2019-04-15 03:11:41 +0530

    update status is working now. i have to set the user. it is hardcoded to mayank343 for now

--- app/src/main/java/com/example/merchandiseapp/productsCourrier.java
+++ app/src/main/java/com/example/merchandiseapp/productsCourrier.java
@@ -1,5 +1,6 @@
 package com.example.merchandiseapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +13,17 @@ import android.widget.TextView;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.merchandiseapp.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class productsCourrier extends AppCompatActivity {
     private ImageView productImage;
@@ -24,10 +31,10 @@ public class productsCourrier extends AppCompatActivity {
     private TextView productPrice, productName;
     private String productID = "";
     private String User_ID = "";
-    private String orderID = "";
+    private String OrderID = "";
     private String date = "";
     private String time = "";
-
+    private String status = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +77,8 @@ public class productsCourrier extends AppCompatActivity {
                 System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@2"+date+" "+time+"@@@@@@@@@@@@@@@@@@@@");
                 Order order = dataSnapshot.getValue(Order.class);
                 //System.out.println("hello" + order);
-
+                status = order.getStatus();
+                OrderID = order.getOrderid();
                productName.setText(order.getPname());
                productPrice.setText(order.getPrice());
 
@@ -91,6 +99,27 @@ public class productsCourrier extends AppCompatActivity {
 
     public void doOnClick(View view)
     {
+        System.out.println("dsaaaaaaaa");
+        Spinner dropdown = findViewById(R.id.spinner1);
+        String text = dropdown.getSelectedItem().toString();
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("status", text);
+        System.out.println("dsaaaaaaaa"+User_ID + OrderID);
+        User_ID="mayank343";
+        cartListRef.child(User_ID).child(OrderID).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+
+                if(task.isSuccessful())
+                {
+                }
+            }
+        });
 
 
 
