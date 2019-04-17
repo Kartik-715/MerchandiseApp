7a22d6a72ec52bca6585c7f4dfc3201f5a29661c
mnk343 <mayank343@gmail.com>
2019-04-17 21:04:41 +0530

    hello

--- app/src/main/java/com/example/merchandiseapp/multiple_images.java
+++ app/src/main/java/com/example/merchandiseapp/multiple_images.java
@@ -1,8 +1,12 @@
 package com.example.merchandiseapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +14,18 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class multiple_images extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE1 = 1;
     private Button SelectedButton;
     private RecyclerView UploadList;
 
+    private List<String> fileNameList;
+    private List<String> fileDoneList;
+    private UplaodListAdapter uplaodListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,14 @@ public class multiple_images extends AppCompatActivity {
 
         SelectedButton = (Button) findViewById(R.id.addImg);
         UploadList = (RecyclerView) findViewById( R.id.recyclerview);
+
+        fileNameList = new ArrayList<>();
+        fileDoneList = new ArrayList<>();
+
+        uplaodListAdapter = new UplaodListAdapter(fileNameList , fileDoneList );
+        UploadList.setLayoutManager(new LinearLayoutManager(this));
+//        UploadList.hasFixedSize("true");
+        UploadList.setAdapter(uplaodListAdapter);
 
         SelectedButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,24 +66,65 @@ public class multiple_images extends AppCompatActivity {
 
             if(data.getClipData() != null)
             {
+                int totalItemSelected =data.getClipData().getItemCount();
 
+                for(int i=0 ;i <totalItemSelected;i++)
+                {
+                    Uri fileUri = data.getClipData().getItemAt(i).getUri();
+                    String fileName = getFileName(fileUri);
+
+                    fileNameList.add(fileName);
+                    uplaodListAdapter.notifyDataSetChanged();
+                }
             }
 
             else if (data.getData() != null)
             {}
 
-            else
-            {
 
-
-
-            }
 
         }
 
 
     }
 
+public String getFileName(Uri uri){
 
+        String result = null;
+
+        if(uri.getScheme().equals("content")){
+
+            Cursor cursor= getContentResolver().query(uri,null,null,null,null);
+            try{
+
+                if (cursor!=null && cursor.moveToFirst())
+                {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+
+                }
+            }
+
+            finally {
+                cursor.close();
+            }
+
+
+
+        }
+
+        if(result==null)
+        {
+            result=uri.getPath();
+            int cut = result.lastIndexOf('/');
+
+            if(cut != -1){
+
+                result = result.substring(cut+1);
+
+            }
+        }
+
+        return result;
+}
 
 }
