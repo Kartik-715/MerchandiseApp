54337b486fff06ee914d1d133451045251c823fd
Aryan Agrawal <aryanpranshu@gmail.com>
2019-04-18 05:00:24 +0530

    remove preorder implemented

--- app/src/main/java/com/example/merchandiseapp/PreBookings.java
+++ app/src/main/java/com/example/merchandiseapp/PreBookings.java
@@ -1,7 +1,6 @@
 package com.example.merchandiseapp;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -11,17 +10,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.merchandiseapp.Holder.PreBookingHolder;
-import com.example.merchandiseapp.Prevalent.Prevalent;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,7 +26,6 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Properties;
 
 public class PreBookings extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -68,7 +60,8 @@ public class PreBookings extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
     }
 
-    public void exit(View view){
+    public void deleteMerchandise(View view){
+
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         // Setting Alert Dialog Title
         alertDialogBuilder.setTitle("Confirm Remove..!!!");
@@ -85,6 +78,45 @@ public class PreBookings extends AppCompatActivity {
                 preOederListRef.child(PID).child("IsOpen").setValue("false");
 
 
+                myRef = FirebaseDatabase.getInstance().getReference("/Group").child(GroupName).child("Merchandise").child(Category).child(PID);
+                myRef.child("IsOpen").setValue("false");
+                finish();
+                startActivity(getIntent());
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"You clicked over No",Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"You clicked on Cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void makeMerchandisePrivate(View view){
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        // Setting Alert Dialog Title
+        alertDialogBuilder.setTitle("Confirm Remove..!!!");
+        // Icon Of Alert Dialog
+        alertDialogBuilder.setIcon(R.drawable.checked);
+        // Setting Alert Dialog Message
+        alertDialogBuilder.setMessage("Are you sure,You want to remove item");
+        alertDialogBuilder.setCancelable(false);
+
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
                 myRef = FirebaseDatabase.getInstance().getReference("/Group").child(GroupName).child("Merchandise").child(Category).child(PID);
                 myRef.child("IsOpen").setValue("false");
                 finish();
@@ -172,6 +204,10 @@ public class PreBookings extends AppCompatActivity {
 //                startActivity(intent);
 //            }
 //        });
+
+
+        Toast.makeText(getApplicationContext(),"No Pre-Order Requested as of now.",Toast.LENGTH_LONG);
+
     }
 
     private void DataExists(Query queries)
@@ -190,7 +226,7 @@ public class PreBookings extends AppCompatActivity {
                     System.out.println(model);
 
 
-                        holder.txtProductQuantity.setText("Quantity = " + model.getTotalQuantity());
+                        //holder.txtProductQuantity.setText("Quantity = " + model.getTotalQuantity());
                         holder.txtProductPrice.setText("Price: " + model.getPrice());
                         holder.txtProductName.setText(model.getCategory());
                         Picasso.get().load(model.getImage()).into(holder.CartImage);
@@ -204,13 +240,19 @@ public class PreBookings extends AppCompatActivity {
                         holder.DeleteButton.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                System.out.println("*********************it should have entered here");
 
-                                System.out.println(PID+Category);
-                                exit(v);
+                                deleteMerchandise(v);
                             }
 
 
+                        });
+
+                        holder.PrivateButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                makeMerchandisePrivate(view);
+
+                            }
                         });
 
                         holder.RequestButton.setOnClickListener(new View.OnClickListener() {
