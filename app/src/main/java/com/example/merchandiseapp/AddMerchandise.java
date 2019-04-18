20f1426a83169576ed23c1d236933a9c5f4d2ca5
Aryan Agrawal <aryanpranshu@gmail.com>
2019-04-18 14:21:10 +0530

    add merchandise modified

--- app/src/main/java/com/example/merchandiseapp/AddMerchandise.java
+++ app/src/main/java/com/example/merchandiseapp/AddMerchandise.java
@@ -33,7 +33,6 @@ public class AddMerchandise extends AppCompatActivity {
     private EditText access_editText;
     private EditText size_edt;
     private EditText qty_edt;
-    private EditText gpName;
     private EditText mat;
     private EditText price_edt;
     private EditText prod_Id;
@@ -50,11 +49,13 @@ public class AddMerchandise extends AppCompatActivity {
     private String Material;
     private String PID;
     private String Price;
-    private String OrderType;
+    private String OrderType="1";
 
     ArrayAdapter adapter;
     ArrayAdapter adapterSize_qty;
+
     DatabaseReference myRef ;
+    DatabaseReference myRef2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +75,7 @@ public class AddMerchandise extends AppCompatActivity {
         access_editText = (EditText) findViewById(R.id.editText_accessgroup);
         size_edt = (EditText) findViewById(R.id.editText_size);
         qty_edt = (EditText) findViewById(R.id.editText_qty);
-        gpName = (EditText) findViewById(R.id.EditText_GroupName);
+
         mat = (EditText) findViewById(R.id.EditText_Material);
         price_edt = (EditText) findViewById(R.id.EditText_Price);
         prod_Id = (EditText) findViewById(R.id.EditText_prodID);
@@ -82,13 +83,14 @@ public class AddMerchandise extends AppCompatActivity {
         accessGroupListView = (ListView) findViewById(R.id.listView_accessgroup);
         sizeQtyListView =(ListView) findViewById(R.id.listView_size_qty);
 
+        GroupName = "CSEA";
 
 
 
 
 
 
-        AccessGroups.add("CSEA");
+        AccessGroups.add(GroupName);
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,AccessGroups);
         adapterSize_qty  = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listSQ);
@@ -96,6 +98,10 @@ public class AddMerchandise extends AppCompatActivity {
         add_accessgroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(access_editText.getText().toString()=="")
+                {
+                    return;
+                }
                 AccessGroups.add(access_editText.getText().toString());
                 adapter.notifyDataSetChanged();
                 access_editText.setText("");
@@ -117,7 +123,7 @@ public class AddMerchandise extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Enter the quantity", Toast.LENGTH_LONG);
                     return;
                 }
-                listSQ.add(size_edt.getText().toString()+":"+qty_edt.getText().toString());
+                listSQ.add("Size="+size_edt.getText().toString()+":Qty="+qty_edt.getText().toString());
                 size.add(size_edt.getText().toString());
                 qty.add(qty_edt.getText().toString());
                 size_edt.setText("");
@@ -131,7 +137,6 @@ public class AddMerchandise extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Spinner dropdown = findViewById(R.id.spinner1);
-                GroupName = gpName.getText().toString();
                 Category = (String) dropdown.getSelectedItem();
                 Image = new ArrayList<>();
                 Material = mat.getText().toString();
@@ -139,15 +144,43 @@ public class AddMerchandise extends AppCompatActivity {
                 Price = price_edt.getText().toString();
                 System.out.println(GroupName+Category+Image+Material+PID+Price);
 
+                if(listSQ.size()==0)
+                {
+                    Toast.makeText(getApplicationContext(),"Enter Valid Quantity and Size",Toast.LENGTH_LONG);
+                    return;
+                }
+                if(PID=="")
+                {
+                    Toast.makeText(getApplicationContext(),"Enter Valid Product ID",Toast.LENGTH_LONG);
+                    return;
+                }
+                if(Price=="")
+                {
+                    Toast.makeText(getApplicationContext(),"Enter a Valid Price",Toast.LENGTH_LONG);
+                    return;
+                }
+
                 myRef = FirebaseDatabase.getInstance().getReference().child("Group").child("CSEA").child("Merchandise");
+                myRef2 = FirebaseDatabase.getInstance().getReference().child("Merchandise");
                 Merchandise merchandise = new Merchandise(GroupName,Category,Image,Material,PID,Price,qty,size,AccessGroups,OrderType,"true");
                 HashMap<String, Object> merchandiseValues = merchandise.toMap();
 
                 HashMap<String, Object> childUpdates = new HashMap<>();
+
                 childUpdates.put( Category +"/"+PID, merchandiseValues);
 
 
+
                 myRef.updateChildren(childUpdates);
+                myRef2.updateChildren(childUpdates);
+
+                access_editText.setText("");
+                size_edt.setText("");
+                qty_edt.setText("");
+                mat.setText("");
+                price_edt.setText("");
+                prod_Id.setText("");
+
 
             }
         });
@@ -161,11 +194,11 @@ public class AddMerchandise extends AppCompatActivity {
             case R.id.radioButtonYes:
                 if (checked)
                     OrderType = "1";
-                    break;
+                break;
             case R.id.radioButtonNO:
                 if (checked)
                     OrderType = "2";
-                    break;
+                break;
         }
     }
 
